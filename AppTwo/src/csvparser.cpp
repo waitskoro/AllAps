b867256533d0bed31a7f8b8e7181999281480448
Guryanova.Kristina <cristina.guryanova2017@yandex.ru>
2025-06-11 16:03:42 +0700

    feat: Сокращение файла если он превышает 55 мб

--- AppTwo/src/csvparser.cpp
+++ AppTwo/src/csvparser.cpp
@@ -3,8 +3,9 @@
 #include <QFile>
 #include <QDebug>
 #include <QThread>
-#include <QStandardItem>
 #include <QTextStream>
+#include <QStandardItem>
+#include <QTemporaryFile>
 
 using namespace Reports;
 
@@ -14,8 +15,11 @@ CsvParser::CsvParser(QObject *parent)
 {
     for (int i = 0; i < CHANNEL_COUNT; ++i) {
         QString filename = QString("../reports_%1.csv").arg(i+1);
-        m_files[i] = fopen(filename.toLocal8Bit().constData(), "a");
-        if (!m_files[i]) {
+        m_files[i] = new QFile(filename);
+        if (!m_files[i]->open(QIODevice::Append |
+                              QIODevice::Text |
+                              QIODevice::Truncate |
+                              QIODevice::ReadWrite)) {
             qDebug() << "Failed to open file:" << filename;
         }
     }
@@ -28,43 +32,73 @@ CsvParser::~CsvParser()
 {
     for (int i = 0; i < CHANNEL_COUNT; ++i) {
         if (m_files[i]) {
-            fclose(m_files[i]);
+            m_files[i]->close();
+            delete m_files[i];
         }
     }
 }
 
-bool CsvParser::appendChannelData(int channelNumber,
+void CsvParser::appendChannelData(int channelNumber,
                                   double iQuadrature,
                                   double qQuadrature)
 {
     if (channelNumber < 1 || channelNumber > CHANNEL_COUNT) {
         qDebug() << "Invalid channel number:" << channelNumber;
-        return false;
+        return;
     }
 
-    FILE* file = m_files[channelNumber - 1];
-    if (!file) {
+    QFile* file = m_files[channelNumber - 1];
+    if (!file || !file->isOpen()) {
         qDebug() << "File for channel" << channelNumber << "is not open";
-        return false;
+        return;
     }
 
     QString str = QString::number(iQuadrature, 'f', 6) + ";" +
                   QString::number(qQuadrature, 'f', 6) + "\n";
 
-    return write(str, file);
+    write(str, file);
 }
 
-bool CsvParser::write(QString str, FILE *__restrict file)
+void CsvParser::write(QString str, QFile* file)
 {
-    QByteArray byteArray = str.toUtf8();
-    const char *buffer = byteArray.constData();
-    size_t bytesToWrite = byteArray.size();
-
-    size_t written = fwrite(buffer, 1, bytesToWrite, file);
-    if (written != bytesToWrite) {
-        qDebug() << "Failed to write all data";
-        return false;
+    if (file->size() > 55500000) {
+        removeFirstNLines(file, 500000);
     }
 
-    return true;
+    QTextStream stream(file);
+    stream << str;
+
+    if (stream.status() != QTextStream::Ok) {
+        qDebug() << "Failed to write data";
+    }
+}
+
+void CsvParser::removeFirstNLines(QFile* file, int n) {
+    if (!file || n <= 0 || !file->isOpen()) return;
+
+    file->close();
+    if (!file->open(QIODevice::ReadWrite | QIODevice::Text)) {
+        qDebug() << "Failed to reopen file for truncation";
+        return;
+    }
+
+    QTextStream in(file);
+    QString content;
+    int linesSkipped = 0;
+
+    while (linesSkipped < n && !in.atEnd()) {
+        in.readLine();
+        linesSkipped++;
+    }
+
+    content = in.readAll();
+    file->resize(0);
+
+    QTextStream out(file);
+    out << content;
+    file->close();
+
+    if (!file->open(QIODevice::Append | QIODevice::Text | QIODevice::ReadWrite)) {
+        qDebug() << "Failed to reopen file in append mode";
+    }
 }
--- AppTwo/src/csvparser.h
+++ AppTwo/src/csvparser.h
@@ -24,26 +24,15 @@ public:
     explicit CsvParser(QObject *parent = nullptr);
     ~CsvParser();
 
-    bool appendChannelData(int channelNumber, double iQuadrature, double qQuadrature);
+    void appendChannelData(int channelNumber, double iQuadrature, double qQuadrature);
 
 private:
-    FILE *m_fileCh1;
-    FILE *m_fileCh2;
-    FILE *m_fileCh3;
-    FILE *m_fileCh4;
-    FILE *m_fileCh5;
-    FILE *m_fileCh6;
-    FILE *m_fileCh7;
-    FILE *m_fileCh8;
-    FILE *m_fileCh9;
-    FILE *m_fileCh10;
-    FILE *m_fileCh11;
-    FILE *m_fileCh12;
-
     QThread *m_thread;
 
-    FILE* m_files[CHANNEL_COUNT];
-    bool write(QString, FILE *__restrict);
+    QFile* m_files[CHANNEL_COUNT];
+
+    void write(QString, QFile *__restrict);
+    void removeFirstNLines(QFile* file, int n);
 };
 
 }
